1b445cd82449ee773b0116a0c3a752a8cb5b3734
Ken Dreyer <kdreyer@redhat.com>
2020-12-04 15:46:20 -0700

    setup.py: fix closing bracket indentation
    Fixes flake8 error:
    E124 closing bracket does not match visual indentation

--- setup.py
+++ setup.py
@@ -12,28 +12,29 @@ metadata = dict(re.findall(r"__([a-z]+)__\s*=\s*'([^']+)'", module_file))
 version = metadata['version']
 
 
-setup(name="helga-bugzilla",
-      version=version,
-      description=('bugzilla plugin for helga'),
-      classifiers=['Development Status :: 4 - Beta',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development :: Bug Tracking',
-                   ],
-      keywords='irc bot bugzilla',
-      author='ken dreyer',
-      author_email='ktdreyer@ktdreyer.com',
-      url='https://github.com/ktdreyer/helga-bugzilla',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=[
-          'helga',
-          'txbugzilla>=1.4.0',
-      ],
-      entry_points=dict(
-          helga_plugins=[
-              'bugzilla = helga_bugzilla:helga_bugzilla',
-          ],
-      ),
+setup(
+    name="helga-bugzilla",
+    version=version,
+    description=('bugzilla plugin for helga'),
+    classifiers=['Development Status :: 4 - Beta',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Bug Tracking',
+                 ],
+    keywords='irc bot bugzilla',
+    author='ken dreyer',
+    author_email='ktdreyer@ktdreyer.com',
+    url='https://github.com/ktdreyer/helga-bugzilla',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'helga',
+        'txbugzilla>=1.4.0',
+    ],
+    entry_points=dict(
+        helga_plugins=[
+            'bugzilla = helga_bugzilla:helga_bugzilla',
+        ],
+    ),
 )
